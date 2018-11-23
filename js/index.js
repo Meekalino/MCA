eed8da7a5df739579ffd30e62ef50fc90c2fd46e
Kirsty <kirstymeek@gmail.com>
2018-11-23 14:27:56 +0000

    Added MEI file information.

--- js/index.js
+++ js/index.js
@@ -20,4 +20,4 @@ options = {
 vrvToolkit.setOptions(options)
 
 /* Show the score */
-showScore('score', 'data/example.mei') // <-- CHANGE THIS TO YOUR FILE
+showScore('score', 'Dark_BlueXML.mei') // <-- CHANGE THIS TO YOUR FILE
